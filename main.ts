85dad3c7a71345cbeaf14d119b1e90af85654c96
John Reinier Bulaong <rein.bulaong@yahoo.com>
2025-07-30 02:13:46 +0800

    add logging and cors

--- main.ts
+++ main.ts
@@ -1,12 +1,20 @@
 import { Hono } from '@hono/hono'
+import { logger } from '@hono/hono/logger'
+import { cors } from "@hono/hono/cors"
 import * as rawg from "./src/rawg/mod.ts";
 
 const app = new Hono()
+
+app.use(logger());
+app.use(cors({
+  origin: Deno.env.get("CORS_ORIGIN")!
+}))
 
 app.post("/get", async (ctx) => {
   const body = await ctx.req.json();
   const id: number = body.id;
   const game = await rawg.get(id);
+  console.log(`${id} - ${game.title}`)
   return ctx.json(game)
 })
 
@@ -14,6 +22,7 @@ app.post("/search", async (ctx) => {
   const body = await ctx.req.json();
   const title: string = body.title;
   const games = await rawg.search(title);
+  console.log(`${title} - ${games.map(g => g.id)} (${games.length})`)
   return ctx.json(games)
 })
 
